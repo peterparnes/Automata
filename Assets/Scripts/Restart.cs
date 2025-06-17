8c2d3a3ffa64cff85105410af41fad622f926a53
Peter Parnes <peter@parnes.com>
2025-06-17 13:43:14 +0200

    fix: handle missing automata object in Restart script

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -9,11 +9,18 @@ public class Restart : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        if (automata == null)
+        {
+            Debug.LogError("Automata object is missing");
+            return;
+        }
+
         if (Input.GetKeyDown(KeyCode.R))
         {
             automata.SetActive(false);
             automata.SetActive(true);
-        } else if (Input.GetKeyDown(KeyCode.Q))
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
         {
             Quit();
         }
